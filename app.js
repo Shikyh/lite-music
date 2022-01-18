e80d3216a81d580aabdc6982f1049a7781a83cf5
Shikyh <serlia@sina.com>
2022-01-18 20:10:07 +0800

    feat:歌曲详情页开发

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
 // app.js
 App({
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-  },
-  globalData: {
-    userInfo: null
-  }
-})
+	onLaunch() {},
+	globalData: {
+		screenWidth: 0,
+		screenHeight: 0,
+		statusBarHeight: 0,
+		navBarHeight: 44, //导航栏占位高度
+		deviceRadio: 0,
+	},
+});
---
+++ components/song-detail-header/index.js
@@ -0,0 +1,13 @@
+// components/song-detail-header/index.js
+Component({
+	properties: {
+		songInfo: {
+			type: Object,
+			value: {},
+		},
+	},
+
+	data: {},
+
+	methods: {},
+});
